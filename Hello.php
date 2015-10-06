d7b72c3ab5b23e9c716c7d186e9073ac6683f0d1
Paolo Davila <paolodavila@iesebre.com>
2015-10-06 19:37:05 +0200

    My first helloWorld with Composer

---
+++ Hello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdavila
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+
+class Hello {
+
+    public function say_hello()
+    {
+        echo "Hello world! \n";
+    }
+
+}
---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/Hello.php";
+
+/**
+ * Created by PhpStorm.
+ * User: pdavila
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+
+$greetings = new Hello();
+
+$greetings->say_hello();
